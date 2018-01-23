1c6e0d4ac791bc5426e75436bed51e26c35eb865
liuweijw <liuweijw.github@foxmail.com>
2018-01-24 00:38:03 +0800

    init first project and config

---
+++ firstlibrary/src/main/java/com/liuweijw/library/first/TestUtils.java
@@ -0,0 +1,12 @@
+package com.liuweijw.library.first;
+
+/**
+ * Created by LW on 2018/1/23.
+ */
+
+public class TestUtils {
+
+    public static void test(){
+        System.out.print(" hello world !!!");
+    }
+}
